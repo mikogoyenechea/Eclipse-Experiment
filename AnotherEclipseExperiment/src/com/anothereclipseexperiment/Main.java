2f774980824d653c8d7419375856daac66c19dcf
mikogoyenechea <mikogoyenechea@gmail.com>
2022-04-27 17:41:27 +0800

    Initial commit

---
+++ AnotherEclipseExperiment/src/com/anothereclipseexperiment/Main.java
@@ -0,0 +1,15 @@
+package com.anothereclipseexperiment;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		methodOne();
+
+	}
+	
+	public static void methodOne () {
+		System.out.println("This is another project");
+	}
+
+}
