b51553aad5a9160c3f633f7063394604716e349d
mikogoyenechea <mikogoyenechea@gmail.com>
2022-04-27 17:13:31 +0800

    Initial commit

---
+++ EclipseExperiment/src/com/eclipseexperiment/Main.java
@@ -0,0 +1,15 @@
+package com.eclipseexperiment;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		methodOne();
+		
+	}
+	
+	public static void methodOne () {
+		System.out.println("This is methodOne from com.eclipseexperiment.");
+	}
+
+}
---
+++ EclipseExperiment/src/com/eclipseexperiment2/Main.java
@@ -0,0 +1,16 @@
+package com.eclipseexperiment2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		methodOne();
+		
+	}
+	
+	public static void methodOne () {
+		System.out.println("This is methodOne from com.eclipseexperiment2.");
+	}
+
+
+}
