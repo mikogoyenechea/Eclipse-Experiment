c13ff572311e6cca375949f15a7731523a9644b5
mikogoyenechea <mikogoyenechea@gmail.com>
2022-04-28 21:38:54 +0800

    Added another method.

--- EclipseExperiment/src/com/eclipseexperiment/Main.java
+++ EclipseExperiment/src/com/eclipseexperiment/Main.java
@@ -5,11 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 
 		methodOne();
+		methodTwo();
 		
 	}
 	
 	public static void methodOne () {
 		System.out.println("This is methodOne from com.eclipseexperiment.");
+	}
+	
+	public static void methodTwo () {
+		System.out.println("I added another method and uploaded the changes to GitHub.");
 	}
 
 }
